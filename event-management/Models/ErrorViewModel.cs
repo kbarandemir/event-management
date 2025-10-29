d77d21579f8feb8300b7ed8f3e68f6c06f3dcc93
Kamber Baran Demir <k.barandemir@gmail.com>
2025-10-29 08:28:02 +0000

    Beginning of project

---
+++ event-management/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace event_management.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
